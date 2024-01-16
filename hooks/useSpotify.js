374a47b96547b8713f9187d4af3e1d46d30ef7d2
Lennart Kaminsky <lennartkaminsky@googlemail.com>
2024-01-16 19:27:52 +0100

    top artists and tracks

---
+++ hooks/useSpotify.js
@@ -0,0 +1,24 @@
+import { useSession, signIn } from "next-auth/react";
+import { useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-node";
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+});
+
+export default function useSpotify() {
+  const { data: session, status } = useSession();
+
+  useEffect(() => {
+    if (session) {
+      //if refresh access token access fails
+      if (session.error === "RefreshAccessTokenError") {
+        signIn();
+      }
+      spotifyApi.setAccessToken(session.user.accessToken);
+    }
+  }, [session]);
+
+  return spotifyApi;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,45 @@
+import useSpotify from "@/hooks/useSpotify";
 import { useSession, signOut, signIn } from "next-auth/react";
-import spotifyApi from "@/lib/spotify";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const { data: session, status } = useSession();
-  console.log("status", status);
-  if (status === "authenticated") {
-    console.log("session", session);
-  }
+  const mySpotifyApi = useSpotify();
+  const [topArtists, setTopArtists] = useState([]);
+  const [topTracks, setTopTracks] = useState([]);
+
+  // useEffect(() => {
+  //   if (mySpotifyApi.getAccessToken()) {
+  //     mySpotifyApi.getMyTopArtists({ time_range: "long_term" }).then(
+  //       function (data) {
+  //         setTopArtists(data.body.items);
+  //       },
+  //       function (err) {
+  //         console.log("Something went wrong!", err);
+  //       }
+  //     );
+  //   }
+  // }, [session, mySpotifyApi]);
+
+  useEffect(() => {
+    async function getTopArtistsTracks() {
+      try {
+        if (mySpotifyApi.getAccessToken()) {
+          const topArtists = await mySpotifyApi.getMyTopArtists({
+            time_range: "long_term",
+          });
+          setTopArtists(topArtists.body.items);
+          const topTracks = await mySpotifyApi.getMyTopTracks({
+            time_range: "long_term",
+          });
+          setTopTracks(topTracks.body.items);
+        }
+      } catch (error) {
+        console.error("Something went wrong!", error);
+      }
+    }
+    getTopArtistsTracks();
+  }, [session, mySpotifyApi]);
 
   return (
     <main>
@@ -16,6 +49,17 @@ export default function Home() {
           <p>Hi {session.user.name}</p>{" "}
           <button onClick={() => signOut()}>Sign Out</button>
           <h2>My Top Artists</h2>
+          <ol>
+            {topArtists.map((artist) => (
+              <li key={artist.id}>{artist.name}</li>
+            ))}
+          </ol>
+          <h2>My Top Tracks</h2>
+          <ol>
+            {topTracks.map((tracks) => (
+              <li key={tracks.id}>{tracks.name}</li>
+            ))}
+          </ol>
         </>
       ) : (
         <>
